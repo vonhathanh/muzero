5b90f3de16ce80b984f1b0dacfaeecddd2f2b342
hanhvn <vonhathanh@gmail.com>
2025-03-22 19:46:45 +0700

    restart muzero from scratch

--- constants.py
+++
@@ -1,5 +0,0 @@
-import collections
-
-MAXIMUM_FLOAT_VALUE = float('inf')
-
-KnownBounds = collections.namedtuple('KnownBounds', ['min', 'max'])
--- shared_storage.py
+++
@@ -1,9 +0,0 @@
-class SharedStorage:
-    def __init__(self):
-        self.network = None
-
-    def get_latest_checkpoint(self):
-        return self.network
-
-    def save_network(self, net):
-        self.network = net
